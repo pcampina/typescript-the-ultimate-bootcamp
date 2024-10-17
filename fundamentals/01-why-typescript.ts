5bb8d68b1d214d7cf74956cb7695a4eb3a8c505a
pcampina <pablo@campina.dev>
2024-10-17 13:11:31 +0100

    Compiling Your First Typescript Program

---
+++ fundamentals/01-why-typescript.ts
@@ -0,0 +1,7 @@
+const courseName = "Typescript Bootcamp";
+
+printCourseName(courseName);
+
+function printCourseName(name: string) {
+    console.log("The course name is " + name.toUpperCase());
+}
